056bb68b0e6f5855e2acf0083ffb56b15896cc4b
Zokor <bflgomes@gmail.com>
2016-04-04 11:06:09 +0100

    Inicial commit
    Original versions

---
+++ height-aligner.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+	setTimeout(function () {
+		var uniformHeight = 0, itemHeight = 0;
+		$(".height-align").each(function (index) {
+			itemHeight = $(this).height();
+			if (itemHeight > uniformHeight) {
+				uniformHeight = itemHeight;
+			}
+		});
+		$(".height-align").height(uniformHeight);
+	}, 0);
+});
